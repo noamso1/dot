6a8f96abd0ccb9f230aae17b5e40547e88198765
noamso1 <noamso@gmail.com>
2023-10-25 07:21:45 +0700

    .

--- setup.sh
+++ setup.sh
@@ -17,6 +17,7 @@ NODE_MAJOR=18
 echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list
 sudo apt-get update
 sudo apt-get install nodejs -y
+node -v
 
 #=================nvim ===neovim
 ### install nvim as appimage
