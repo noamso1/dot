c15154cbc5fa6036810a2fb5d5f096291393aa05
noamso1 <noamso@gmail.com>
2024-02-14 14:45:06 +0700

    .

--- setup.sh
+++ setup.sh
@@ -9,15 +9,7 @@ cp ~/dot/tmux.conf ~/.tmux.conf
 cp ~/dot/nanorc ~/.nanorc.conf
 
 #====================nodejs
-sudo apt-get update
-sudo apt-get install -y ca-certificates curl gnupg
-sudo mkdir -p /etc/apt/keyrings
-curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
-NODE_MAJOR=18
-echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list
-sudo apt-get update
-sudo apt-get install nodejs -y
-node -v
+installnode #function from .bashrc
 sudo npm install -g pm2 mongodb json5
 
 #=================nvim ===neovim
