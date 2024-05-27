26aa6519249e5573243f6716dfeec01fe29ccd3c
noamso1 <noamso@gmail.com>
2024-05-27 13:51:09 +0700

    .

--- setup.sh
+++ setup.sh
@@ -1,15 +1,21 @@
 # curl https://raw.githubusercontent.com/noamso1/dot/master/setup.sh | bash
+
 set -e ; set -x ; cd ~
+
 sudo apt update -y && sudo apt upgrade -y
 sudo apt install -y htop curl git tmux nginx docker.io neovim nodejs npm gcc unzip ripgrep
+
 sudo mv /bin/vi /bin/vi.old
 sudo ln -s /bin/nvim /bin/vi
+
 git clone https://github.com/noamso1/dot.git # git clone git@github.com:noamso1/dot.git
 cp ~/dot/bashrc ~/.bashrc
 cp ~/dot/tmux.conf ~/.tmux.conf
 cp ~/dot/nanorc ~/.nanorc
+
 sudo npm install -g pm2 mongodb
 cd ~/dot && npm i json5 && cd -
+
 rm -rf ~/.config/nvim ~/.local/share/nvim ~/.cache/nvim
 mkdir -p ~/.config/nvim
 tar xf ~/dot/nvim_config.tar.xz -C ~/.config/nvim
