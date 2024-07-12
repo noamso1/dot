0779cc93bbcce2ff8b256f2a9e077bc0da63e44c
noamso <noamso@gmail.com>
2024-07-12 08:48:37 +0700

    .

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@
 set -e ; set -x ; cd ~
 
 sudo apt update -y && sudo apt upgrade -y
-sudo apt install -y htop curl git tmux nginx docker.io neovim nodejs npm gcc unzip ripgrep
+sudo apt install -y htop curl git tmux nginx docker.io neovim nodejs npm gcc unzip ripgrep jq
 
 sudo mv /bin/vi /bin/vi.old
 sudo ln -s /bin/nvim /bin/vi
