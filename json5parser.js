a82e274ab5c570b12f0428b8800876af3b0bee82
noamso <noamso@gmail.com>
2025-06-09 10:21:13 +0300

    .

--- json5parser.js
+++ json5parser.js
@@ -1,6 +1,8 @@
 const JSON5 = require('json5')
 const fs = require("fs");
 const stdinBuffer = fs.readFileSync(0)
-const s = stdinBuffer.toString()
+let s = stdinBuffer.toString()
+s = s.replace(/:\s*undefined\b/g, ': null');
 const j = JSON5.parse(s)
 console.log(JSON.stringify(j, null, 2))
+
