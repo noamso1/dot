df96e6fc5230ce62678adb8d97e74f6709cb27c7
noamso <noamso@gmail.com>
2025-12-29 08:58:20 +0200

    .

--- func.js
+++ func.js
@@ -494,14 +494,12 @@ global.milliTimer = function( a ) {
   if ( prev ) return Number( now - prev ) / 1000000
 }
 
-// // example of promise all tasks with chunks
-// let tasks = [], aa = [ 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22 ]
-// for ( let a of aa ) {
-//   let u = 'http://localhost:8888/?s=' + a
-//   tasks.push({ type: 'test', u, promise: fetchText(u) })
-// }
-// for ( let i = 0; i < tasks.length; i += 20 ) {
-//   let results = await Promise.all(tasks.slice( i, i + 20 ).map((e) => e.promise))
-//   for (let j = 0; j < results.length; j++) { tasks[i + j].result = results[j] }
-// }
-
+// promise all tasks with chunks
+// tasks = [ { id:1, promise: fetchText('https://noamso.one') } ]
+global.executeAll = async function( tasks ) {
+  const chunkSize = 20
+  for ( let i = 0; i < tasks.length; i += chunkSize ) {
+    let results = await Promise.all(tasks.slice( i, i + chunkSize ).map((e) => e.promise))
+    for (let j = 0; j < results.length; j++) { tasks[i + j].result = results[j] }
+  }
+}
