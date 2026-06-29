6bf410a45fd34c7e7dec1e9ea044d39a9db4121f
noamso <noamso@gmail.com>
2026-06-29 13:37:25 +0300

    .

--- setup.sh
+++ setup.sh
@@ -53,6 +53,6 @@ rm -rf ~/.config/nvim ~/.local/share/nvim ~/.cache/nvim && mkdir -p ~/.config/nv
 # reboot
 
 # curl -fsSL https://claude.ai/install.sh | bash
-# GIT_SSH_COMMAND='ssh -i ~/.ssh/noamorq -o IdentitiesOnly=yes' git clone git@github.com:noam-orq/orq.git                                                                                                          
+# GIT_SSH_COMMAND='ssh -i ~/.ssh/noamorq -o IdentitiesOnly=yes' git clone git@github.com:noam-orq/orq.git
 # git config core.sshCommand 'ssh -i ~/.ssh/noamorq'
 
